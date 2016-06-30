ffe86ed954583aef15e0781afd0ca034b9b85e14
junjie tang <arspires7@hotmail.com>
2016-06-30 15:52:48 +0200

    add stock history model

---
+++ src/main/java/com/junjie/model/StockHistory.java
@@ -0,0 +1,160 @@
+package com.junjie.model;
+
+import java.util.Date;
+
+public class StockHistory
+{
+	private Long id;
+	private Date date;
+	private Float open;
+	private Float high;
+	private Float low;
+	private Float close;
+	private Long volume;
+	private Float adjColse;
+
+	public StockHistory(Long id, Date date, Float open, Float high, Float low, Float close, Long volume, Float adjColse)
+	{
+		super();
+		this.id = id;
+		this.date = date;
+		this.open = open;
+		this.high = high;
+		this.low = low;
+		this.close = close;
+		this.volume = volume;
+		this.adjColse = adjColse;
+	}
+	public Long getId()
+	{
+		return id;
+	}
+	public void setId(Long id)
+	{
+		this.id = id;
+	}
+	public Float getOpen()
+	{
+		return open;
+	}
+	public void setOpen(Float open)
+	{
+		this.open = open;
+	}
+	public Float getHigh()
+	{
+		return high;
+	}
+	public void setHigh(Float high)
+	{
+		this.high = high;
+	}
+	public Float getLow()
+	{
+		return low;
+	}
+	public void setLow(Float low)
+	{
+		this.low = low;
+	}
+	public Float getClose()
+	{
+		return close;
+	}
+	public void setClose(Float close)
+	{
+		this.close = close;
+	}
+	public Float getAdjColse()
+	{
+		return adjColse;
+	}
+	public void setAdjColse(Float adjColse)
+	{
+		this.adjColse = adjColse;
+	}
+	public Date getDate()
+	{
+		return date;
+	}
+	public void setDate(Date date)
+	{
+		this.date = date;
+	}
+	public Long getVolume()
+	{
+		return volume;
+	}
+	public void setVolume(Long volume)
+	{
+		this.volume = volume;
+	}
+	@Override
+	public String toString()
+	{
+		return String.format("StockHistory [id: %d, date: %s, open: %f, high: %f, low: %f, close: %f, volume: %d, adjClose: %f]", id, date, open,
+			high, low, close, volume, adjColse);
+	}
+	public static Builder builder()
+	{
+		return new Builder();
+	}
+
+	public static class Builder
+	{
+		private Long id;
+		private Date date;
+		private Float open;
+		private Float high;
+		private Float low;
+		private Float close;
+		private Long volume;
+		private Float adjClose;
+
+		public Builder id(Long id)
+		{
+			this.id = id;
+			return this;
+		}
+		public Builder date(Date date)
+		{
+			this.date = date;
+			return this;
+		}
+		public Builder open(Float open)
+		{
+			this.open = open;
+			return this;
+		}
+		public Builder high(Float high)
+		{
+			this.high = high;
+			return this;
+		}
+		public Builder low(Float low)
+		{
+			this.low = low;
+			return this;
+		}
+		public Builder close(Float close)
+		{
+			this.close = close;
+			return this;
+		}
+		public Builder volume(Long volume)
+		{
+			this.volume = volume;
+			return this;
+		}
+		public Builder adjColse(Float adjColse)
+		{
+			this.adjClose = adjColse;
+			return this;
+		}
+		public StockHistory build()
+		{
+			return new StockHistory(id, date, open, high, low, close, volume, adjClose);
+		}
+	}
+
+}
--- src/main/java/com/junjie/sync/SyncStockFromYahooFinance.java
+++ src/main/java/com/junjie/sync/SyncStockFromYahooFinance.java
@@ -1,11 +1,85 @@
 package com.junjie.sync;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+import com.junjie.model.StockHistory;
+import com.junjie.utils.YahooFinanceUtils;
+
 public class SyncStockFromYahooFinance
 {
+	private static boolean SKIP_HEAD_LINE = true;
+	private static String SEPARATOR = ",";
+	private static int NUM_OF_COLUMN = 7;
+	private static SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd");
+
+	public void a()
+	{
+		String targetUrl = YahooFinanceUtils.getDownloadToSpreadsheetURL("000858.SZ");
+		try
+		{
+			URL url = new URL(targetUrl);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream())))
+			{
+				String line;
+				if (SKIP_HEAD_LINE)
+				{
+					br.readLine();
+				}
+				while ((line = br.readLine()) != null)
+				{
+					String[] cells = line.split(SEPARATOR);
+					if (cells.length != NUM_OF_COLUMN)
+					{
+						throw new IllegalArgumentException("number of columns is not " + NUM_OF_COLUMN);
+					}
+
+					StockHistory stockHistory = toStockHistory(cells);
+					System.out.println(stockHistory);
+				}
+
+			}
+		}
+		catch (MalformedURLException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (IOException e)
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	private StockHistory toStockHistory(String[] cells)
+	{
+		try
+		{
+			return StockHistory.builder().date(DATE_FORMAT.parse(cells[0])).open(Float.parseFloat(cells[1])).high(Float.parseFloat(cells[2]))
+				.low(Float.parseFloat(cells[3])).close(Float.parseFloat(cells[4])).volume(Long.parseLong(cells[5]))
+				.adjColse(Float.parseFloat(cells[6])).build();
+		}
+		catch (NumberFormatException e)
+		{
+			e.printStackTrace();
+		}
+		catch (ParseException e)
+		{
+			e.printStackTrace();
+		}
+		return null;
+	}
 
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		new SyncStockFromYahooFinance().a();
 
 	}
 
