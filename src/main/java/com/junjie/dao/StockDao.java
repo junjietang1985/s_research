8fc34c6de8211adf78467bc3682b13bf95073230
junjietang1985 <junjie.tang.1985@gmail.com>
2016-07-03 10:43:07 +0200

    add StockDao.getByStockCode method

--- src/main/java/com/junjie/dao/StockDao.java
+++ src/main/java/com/junjie/dao/StockDao.java
@@ -2,6 +2,7 @@ package com.junjie.dao;
 
 
 
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
@@ -22,6 +23,20 @@ public class StockDao extends JdbcDaoSupport {
 	public int save(Stock stock){
 		String sql="INSERT INTO " + TABLE_NAME + "(stock_code, is_sssz300) VALUES (?, ?)";
 		return getJdbcTemplate().update(sql, new Object[]{stock.getStockCode(),stock.getIsSssz300()});
+	}
+	
+	public Stock getByStockCode(String stockCode){
+
+		String sql = "SELECT * FROM " + TABLE_NAME + " WHERE stock_code = ?";
+
+		try{
+			Stock stock = getJdbcTemplate().queryForObject(sql, new Object[] { stockCode },
+				new BeanPropertyRowMapper<Stock>(Stock.class));
+			return stock;
+		}catch(EmptyResultDataAccessException e){
+			return null;
+		}
+		
 	}
 
 }
