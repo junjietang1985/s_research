7da40b59016aaa8d5d2f162fa8822a89a607d939
junjie tang <junjie.tang.1985@gmail.com>
2016-07-11 15:30:34 +0200

    add get last sync date of stock history

--- src/main/java/com/junjie/dao/StockHistoryDao.java
+++ src/main/java/com/junjie/dao/StockHistoryDao.java
@@ -1,23 +1,30 @@
 package com.junjie.dao;
 
+import java.util.Date;
+
 import org.springframework.jdbc.core.support.JdbcDaoSupport;
 
 import com.junjie.model.StockHistory;
+import com.junjie.utils.DaoUtils;
 
-public class StockHistoryDao extends JdbcDaoSupport {
+public class StockHistoryDao extends JdbcDaoSupport
+{
 
 	private static String TABLE_NAME = "stock_history";
 
-	public int save(StockHistory stockHistory) {
-		String sql = "INSERT INTO "
-				+ TABLE_NAME
-				+ "(stock_code, date, open, high, low, close, volume, adjClose) VALUES (?,?, ?, ?, ?, ?, ?, ?)";
+	public int save(StockHistory stockHistory)
+	{
+		String sql = "INSERT INTO " + TABLE_NAME + " (stock_code, date, open, high, low, close, volume, adjClose) VALUES (?,?, ?, ?, ?, ?, ?, ?)";
 		return getJdbcTemplate().update(
-				sql,
-				new Object[] { stockHistory.getStockCode(),
-						stockHistory.getDate(), stockHistory.getOpen(),
-						stockHistory.getHigh(), stockHistory.getLow(),
-						stockHistory.getClose(), stockHistory.getVolume(),
-						stockHistory.getAdjClose() });
+			sql,
+			new Object[] { stockHistory.getStockCode(), stockHistory.getDate(), stockHistory.getOpen(), stockHistory.getHigh(),
+							stockHistory.getLow(), stockHistory.getClose(), stockHistory.getVolume(), stockHistory.getAdjClose() });
+	}
+
+	public Date getLastSyncDate(String stockCode)
+	{
+		String sql = "SELECT date FROM " + TABLE_NAME + " WHERE stock_code = " + DaoUtils.formatQuatation(stockCode) + " ORDER BY date DESC "
+				+ " LIMIT 1";
+		return getJdbcTemplate().queryForObject(sql, Date.class);
 	}
 }
